24dfaf583677d1e896e1eb402053aaee1b1ce3dd
AgentT30 <cthekkunja304@gmail.com>
2020-08-31 22:35:43 +0530

    Updates

---
+++ precache-manifest.510762fda0525391b7de422c4395f46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beae649b7160635f83b02b3d33d2cd56",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "6db86352a2f1a4f17d33",
+    "url": "/todo-list-react/static/css/main.73407edc.chunk.css"
+  },
+  {
+    "revision": "7902e7ba70777d1ca921",
+    "url": "/todo-list-react/static/js/2.f84e5063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.f84e5063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db86352a2f1a4f17d33",
+    "url": "/todo-list-react/static/js/main.8df04e57.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
